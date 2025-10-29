5b8064d63a2f74d41d767ab04074ce360767bb09
Vignesh A <118335617+Vicky-A-31@users.noreply.github.com>
2025-10-29 18:15:51 +0530

    Create constant.js

---
+++ Constant/constant.js
@@ -0,0 +1,11 @@
+export const ADD_NEW_USER = 'add_new_user';
+
+export const DELETE_USER = 'delete_user';
+
+export const EDIT_USER = 'edit_user';
+
+export const CHANGE_TO_EDIT_MODE = 'change_to_edit_mode';
+
+export const CHANGE_TO_ADD_MODE = 'change_to_add_mode';
+
+export const SEARCHING = 'searching';
